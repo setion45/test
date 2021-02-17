95f719f0817e6f80c38094b77b541217dbab4915
Setion <grifon450@yandex.ru>
2021-02-17 23:20:44 +0400

    '+++'

--- src/main/java/com/example/sweater/controllers/KlientController.java
+++ src/main/java/com/example/sweater/controllers/KlientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -27,4 +28,10 @@ public class KlientController {
      klientRepository.save(klient);
      return "redirect:/";
     }
+//    @PostMapping("/klient/{FIO}/del")
+//    public String del(@PathVariable(value = "FIO") String FIO, Model model) {
+//        Klient klient = klientRepository.findById(FIO).orElseThrow();
+//        klientRepository.delete(klient);
+//        return "redirect:/";
+//    }
 }
